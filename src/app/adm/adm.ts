f210a00035dd9a43847320636d877a8b2c3ae315
GuiSaiUwU <guiguifranco@outlook.com>
2026-04-14 19:54:51 -0300

    todos no admin

--- src/app/adm/adm.ts
+++ src/app/adm/adm.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-adm',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './adm.html',
   styleUrl: './adm.css',
 })
-export class Adm {}
+export class Adm {
+  tarefa: string = '';
+  tarefas: string[] = [];
+
+  adicionarTarefas() {
+    if (this.tarefa.trim() != '') {
+      this.tarefas.push(this.tarefa);
+      this.tarefa = '';
+    }
+  }
+}
